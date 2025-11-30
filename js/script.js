ac35a6f9a0212bac3b17b45738daca9d95c5eb9a
Malaka-Gonawala <malakagonawala123@gmail.com>
2025-11-30 19:39:00 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,22 +3,22 @@ let nav = document.querySelector("#Nav");
 let navOpened = false;
 
 function toggleNav() {
-    nav.classList.toggle("open");
-    btnOpenNav.classList.toggle("active");
-    navOpened = nav.classList.contains("open");
-    fixTitle();
+  nav.classList.toggle("open");
+  btnOpenNav.classList.toggle("active");
+  navOpened = nav.classList.contains("open");
+  fixTitle();
 }
 btnOpenNav.addEventListener("click", toggleNav);
 
 window.addEventListener("click", function (e) {
-    if (nav.classList.contains("open")) {
-        if (!nav.contains(e.target) && !btnOpenNav.contains(e.target)) {
-            nav.classList.remove("open");
-            btnOpenNav.classList.remove("active");
-            navOpened = false;
-            fixTitle();
-        }
+  if (nav.classList.contains("open")) {
+    if (!nav.contains(e.target) && !btnOpenNav.contains(e.target)) {
+      nav.classList.remove("open");
+      btnOpenNav.classList.remove("active");
+      navOpened = false;
+      fixTitle();
     }
+  }
 });
 
 // --------------------------------------------
@@ -27,22 +27,25 @@ let title = document.querySelector("#Title-Container");
 let scrollTimeout;
 
 const fixTitle = () => {
-    if (window.scrollY < 50) {
-        clearTimeout(scrollTimeout);
-        title.classList.remove("start-scroll");
-        title.style.opacity = 1;
-        return;
-    }
-    title.classList.add("start-scroll");
-    title.style.opacity = 1;
-
+  if (window.scrollY < 50) {
     clearTimeout(scrollTimeout);
+    title.classList.remove("start-scroll");
+    title.style.opacity = 1;
+    title.style.pointerEvents = "auto";
+    return;
+  }
+  title.classList.add("start-scroll");
+  title.style.opacity = 1;
+  title.style.pointerEvents = "auto";
 
-    if (navOpened === false) {
-        scrollTimeout = setTimeout(() => {
-            title.style.opacity = 0;
-        }, 1000);
-    }
+  clearTimeout(scrollTimeout);
+
+  if (navOpened === false) {
+    scrollTimeout = setTimeout(() => {
+      title.style.pointerEvents = "none";
+      title.style.opacity = 0;
+    }, 1000);
+  }
 };
 
 window.addEventListener("scroll", fixTitle);
